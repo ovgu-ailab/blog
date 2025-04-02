0b724ba0e8920455e716c5a3c40a63b5dcd6d9af
perschi <73707155+perschi@users.noreply.github.com>
2025-04-02 10:15:07 +0200

    Update sw.js

--- step_ai/sw.js
+++ step_ai/sw.js
@@ -2,7 +2,7 @@ const VERSION = "v1"
 const CACHE_NAME = "AISTEPCOUNTER"
 
 const APP_STATIC_RESOURCES = [
-    "https://ovgu-ailab.github.io/blog/step_ai/",
+    "https://ovgu-ailab.github.io/blog/step_ai/index.html",
     "https://ovgu-ailab.github.io/blog/step_ai/manifest.json",
     "https://ovgu-ailab.github.io/blog/step_ai/icon.svg",
     "https://ovgu-ailab.github.io/blog/step_ai/index.html",
@@ -24,78 +24,70 @@ const APP_STATIC_RESOURCES = [
     "https://ovgu-ailab.github.io/blog/step_ai/figures/phone_gamma.svg",
     "https://ovgu-ailab.github.io/blog/step_ai/figures/phone_t.svg",
     "https://ovgu-ailab.github.io/blog/step_ai/figures/phone_xyz.svg",
-    "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js",
-    "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide-lock.json",
-    "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.asm.wasm",
-    "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/python_stdlib.zip",
-    "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.asm.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js",
-    "https://unpkg.com/reveal.js@^4//dist/reset.css",
-    "https://unpkg.com/reveal.js@^4//dist/reveal.css",
-    "https://unpkg.com/reveal.js@^4//dist/theme/serif.css",
-    "https://unpkg.com/reveal.js@^4//dist/reveal.js",
-    "https://unpkg.com/reveal.js@^4//plugin/notes/notes.js",
-    "https://unpkg.com/reveal.js@^4//plugin/search/search.js",
-    "https://unpkg.com/reveal.js@^4//plugin/zoom/zoom.js",
-    "https://unpkg.com/reveal.js@^4//plugin/math/math.js",
-    "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml-full.js?config=TeX-AMS_HTML-full",
+    //"https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.js",
+    //"https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide-lock.json",
+    //"https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.asm.wasm",
+    //"https://cdn.jsdelivr.net/pyodide/v0.25.0/full/python_stdlib.zip",
+    //"https://cdn.jsdelivr.net/pyodide/v0.25.0/full/pyodide.asm.js",
+    //"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.css",
+    //"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js",
+    //"https://unpkg.com/reveal.js@^4//dist/reset.css",
+    //"https://unpkg.com/reveal.js@^4//dist/reveal.css",
+    //"https://unpkg.com/reveal.js@^4//dist/theme/serif.css",
+    //"https://unpkg.com/reveal.js@^4//dist/reveal.js",
+    //"https://unpkg.com/reveal.js@^4//plugin/notes/notes.js",
+    //"https://unpkg.com/reveal.js@^4//plugin/search/search.js",
+    //"https://unpkg.com/reveal.js@^4//plugin/zoom/zoom.js",
+    //"https://unpkg.com/reveal.js@^4//plugin/math/math.js",
+    //"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml-full.js?config=TeX-AMS_HTML-full",
 ]
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.addAll(resources);
+};
 
-if ('serviceWorker' in navigator) {
-    caches.keys().then(function(cacheNames) {
-        cacheNames.forEach(function(cacheName) {
-            caches.delete(cacheName);
-        });
+const putInCache = async (request, response) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request}) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request.clone());
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    return new Response('Could load neither cache nor Network Response', {
+      status: 404,
+      headers: { 'Content-Type': 'text/plain' },
     });
-}
-// On install, cache the static resources
-self.addEventListener("install", (event) => {
+  }
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(APP_STATIC_RESOURCES);
-    })()
+    addResourcesToCache(APP_STATIC_RESOURCES)
   );
 });
 
-// delete old caches on activate
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      const names = await caches.keys();
-      await Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      );
-      await clients.claim();
-    })()
-  );
-});
-
-// On fetch, intercept server requests
-// and respond with cached responses instead of going to network
-self.addEventListener("fetch", (event) => {
-  // As a single page app, direct app to always go to cached home page.
-  //if (event.request.mode === "navigate") {
-   // event.respondWith(caches.match("/"));
-   // return;
-  //}
-
-  // For all other requests, go to the cache first, and then the network.
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
-        // Return the cached response if it's available.
-        return cachedResponse;
-      }
-      // If resource isn't in the cache, return a 404.
-      return new Response(null, { status: 404 });
-    })()
+    cacheFirst({
+      request: event.request
+    })
   );
 });
